c73768f8af23f415f5e2d067ebb1425e630decbd
Mahmoud Osama <mahmoud.osaamaa@gmail.com>
2026-08-17 14:02:50 +0300

    adding getting logs in database for one item

--- domain/src/main/java/com/example/domain/repository/WorkoutRepository.kt
+++ domain/src/main/java/com/example/domain/repository/WorkoutRepository.kt
@@ -3,5 +3,6 @@ package com.example.domain.repository
 import com.example.domain.model.WorkoutSession
 
 interface WorkoutRepository {
-    suspend fun saveCompletedWorkout(workoutSession: WorkoutSession)
+    suspend fun saveCompletedWorkout(workoutSession: WorkoutSession): Long
+    suspend fun getSavedCompletedWorkout(id: Long): WorkoutSession?
 }
